c726d33c2a412f65d117c2e550cc2b9f093f4164
Linus Johansson <golfarenlj@gmail.com>
2024-07-15 22:02:32 +0000

    Remove unused import.

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post
