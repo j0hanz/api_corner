b618662808482c9b634e42e496a6dac1d6edc47a
Linus Johansson <golfarenlj@gmail.com>
2024-07-16 19:04:26 +0000

    Update root API view to include welcome message and API details

--- api_blog/views.py
+++ api_blog/views.py
@@ -5,8 +5,22 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def root_route(request):
     """
-    Root API view to display a welcome message.
+    Root API view to display a welcome message and API details.
     """
-    return Response(
-        {"message": "This Is the API for the private blog project!"}
-    )
+    api_details = {
+        "message": "Welcome to the API for Corner!",
+        "description": "This is the backend API. Please create an account from the official website.",
+        "endpoints": {
+            "Authentication": "/dj-rest-auth/",
+            "Users": "/users/",
+            "Posts": "/posts/",
+            "Comments": "/comments/",
+            "Likes": "/likes/",
+            "Bookmarks": "/bookmarks/",
+            "Followers": "/followers/",
+            "Reports": "/reports/",
+            "Contact": "/contact/",
+            "News": "/news/",
+        },
+    }
+    return Response(api_details)
