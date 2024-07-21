38ac3f523017e8e95010ec940281e991b7c56c0a
Linus Johansson <golfarenlj@gmail.com>
2024-07-21 19:40:35 +0000

    Update Profile model image field to use Cloudinary for media storage

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -9,8 +10,8 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='images/nobody.webp', blank=True
+    image = CloudinaryField(
+        'image', default='images/nobody_ceyo72', blank=True
     )
     location = models.CharField(max_length=100, blank=True)
     url_link = models.URLField(blank=True)
