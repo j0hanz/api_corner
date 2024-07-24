0a9ba7fccf9991fb0b0455aa0a80165869702686
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 18:23:28 +0000

    Refactor migrations to update generated timestamps

--- api_blog/urls.py
+++ api_blog/urls.py
@@ -17,22 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from .views import root_route
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Blog",
-        default_version='v1',
-        description="API documentation for the Blog project",
-        contact=openapi.Contact(email="contact@apiblog.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -51,14 +36,4 @@ urlpatterns = [
     path('reports/', include('reports.urls')),
     path('contact/', include('contact.urls')),
     path('news/', include('news.urls')),
-    path(
-        'swagger/',
-        schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui',
-    ),
-    path(
-        'redoc/',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc',
-    ),
 ]
