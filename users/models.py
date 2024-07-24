7bc3cf6fcf4cedb05133ecdebd4db11c2c8ae76c
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 14:56:04 +0000

    Refactor Profile model to include owner email in list display

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+# users/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -6,7 +7,9 @@ from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True)
@@ -35,4 +38,5 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
