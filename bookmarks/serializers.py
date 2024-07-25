075a2e5cf6d80069178411ac07fdae3c3f7c9837
Linus Johansson <golfarenlj@gmail.com>
2024-07-25 21:28:44 +0000

    Refactor BookmarkSerializer to include post content, image, and owner information

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -1,18 +1,24 @@
 from rest_framework import serializers
 from .models import Bookmark
+from posts.models import Post
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Bookmark model.
-    """
-
     owner = serializers.ReadOnlyField(source='owner.username')
+    post_content = serializers.ReadOnlyField(source='post.content')
+    post_owner = serializers.ReadOnlyField(source='post.owner.username')
     created_at = serializers.DateTimeField(format='%d %b %Y', read_only=True)
 
     class Meta:
         model = Bookmark
-        fields = ['id', 'owner', 'post', 'created_at']
+        fields = [
+            'id',
+            'owner',
+            'post',
+            'post_content',
+            'post_owner',
+            'created_at',
+        ]
 
     def validate(self, data):
         owner = self.context['request'].user
