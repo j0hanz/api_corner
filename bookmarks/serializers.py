8f8b4d557a15f202954ca41f64e2b33c71c30b6e
Linus Johansson <golfarenlj@gmail.com>
2024-07-25 19:57:09 +0000

    Refactor BookmarkSerializer to prevent duplicate bookmarks

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -3,11 +3,32 @@ from .models import Bookmark
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Bookmark model.
+    Includes logic to prevent duplicate bookmarks.
+    """
+
     owner = serializers.ReadOnlyField(source='owner.username')
     post_content = serializers.CharField(source='post.content', read_only=True)
     post_owner = serializers.CharField(
         source='post.owner.username', read_only=True
     )
+
+    def create(self, validated_data):
+        """
+        Creates a Bookmark instance.
+        Checks if the user already bookmarked the post.
+        Throws an error if the user already bookmarked that instance.
+        """
+        owner = self.context['request'].user
+        post = validated_data.get('post')
+
+        if Bookmark.objects.filter(owner=owner, post=post).exists():
+            raise serializers.ValidationError(
+                'You have already bookmarked this post.'
+            )
+
+        return super().create(validated_data)
 
     class Meta:
         model = Bookmark
