2d33088681b8477ba5e9b67e497440a0434fdfa7
Linus Johansson <golfarenlj@gmail.com>
2024-07-25 19:57:17 +0000

    Refactor BookmarkSerializer to prevent duplicate bookmarks

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from django.db import IntegrityError
 from .models import Bookmark
 from .serializers import BookmarkSerializer
 from api_blog.permissions import IsOwnerOrReadOnly
@@ -24,13 +23,7 @@ class BookmarkListCreateView(generics.ListCreateAPIView):
         return Bookmark.objects.none()
 
     def perform_create(self, serializer):
-        try:
-            serializer.save(owner=self.request.user)
-        except IntegrityError:
-            return Response(
-                {"detail": "You have already bookmarked this post."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        serializer.save(owner=self.request.user)
 
 
 class BookmarkRetrieveDestroyView(generics.RetrieveDestroyAPIView):
