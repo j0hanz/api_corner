4a51bca6b9ff01d10bed61fdbc90a91b13bbc8ff
Linus Johansson <golfarenlj@gmail.com>
2024-07-25 20:56:32 +0000

    Refactor BookmarkSerializer to include post content, image, and owner information

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -5,38 +5,24 @@ from .models import Bookmark
 class BookmarkSerializer(serializers.ModelSerializer):
     """
     Serializer for the Bookmark model.
-    Includes logic to prevent duplicate bookmarks.
     """
 
     owner = serializers.ReadOnlyField(source='owner.username')
-    post_content = serializers.CharField(source='post.content', read_only=True)
-    post_owner = serializers.CharField(
-        source='post.owner.username', read_only=True
-    )
-
-    def create(self, validated_data):
-        """
-        Creates a Bookmark instance.
-        Checks if the user already bookmarked the post.
-        Throws an error if the user already bookmarked that instance.
-        """
-        owner = self.context['request'].user
-        post = validated_data.get('post')
-
-        if Bookmark.objects.filter(owner=owner, post=post).exists():
-            raise serializers.ValidationError(
-                'You have already bookmarked this post.'
-            )
-
-        return super().create(validated_data)
+    created_at = serializers.DateTimeField(format='%d %b %Y', read_only=True)
 
     class Meta:
         model = Bookmark
-        fields = [
-            'id',
-            'owner',
-            'post',
-            'post_content',
-            'post_owner',
-            'created_at',
-        ]
+        fields = ['id', 'owner', 'post', 'created_at']
+
+    def validate(self, data):
+        owner = self.context['request'].user
+        post = data.get('post')
+        if Bookmark.objects.filter(owner=owner, post=post).exists():
+            raise serializers.ValidationError(
+                'You already bookmarked this post'
+            )
+        return data
+
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['request'].user
+        return super().create(validated_data)
