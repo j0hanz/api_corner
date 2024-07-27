506f3d9d1295dffbbcab169add13bd85d44eebaa
Linus Johansson <golfarenlj@gmail.com>
2024-07-27 21:07:39 +0000

    Update dj_database_url.parse() method to use single quotes for consistency

--- api_blog/settings.py
+++ api_blog/settings.py
@@ -127,7 +127,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 # Password validation
