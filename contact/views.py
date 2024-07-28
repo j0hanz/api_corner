fea5b6d287c2b33f344b69152a69627371e9b226
Linus Johansson <golfarenlj@gmail.com>
2024-07-28 21:43:51 +0000

    update views.py

--- contact/views.py
+++ contact/views.py
@@ -4,9 +4,22 @@ from .serializers import ContactSerializer
 from api_blog.permissions import IsOwnerOrReadOnly
 
 
-class ContactListCreateView(generics.ListCreateAPIView):
+class ContactListCreateView(generics.CreateAPIView):
     """
-    View for listing and creating contact messages.
+    View for creating contact messages.
+    """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ContactSerializer
+    throttle_classes = [throttling.UserRateThrottle]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ContactListRetrieveView(generics.ListAPIView):
+    """
+    View for listing contact messages.
     """
 
     permission_classes = [IsOwnerOrReadOnly]
@@ -17,9 +30,6 @@ class ContactListCreateView(generics.ListCreateAPIView):
         return Contact.objects.filter(owner=self.request.user).order_by(
             '-created_at'
         )
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
 
 
 class ContactRetrieveUpdateView(generics.RetrieveUpdateAPIView):
@@ -32,4 +42,4 @@ class ContactRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     throttle_classes = [throttling.UserRateThrottle]
 
     def get_queryset(self):
-        return Contact.objects.filter(owner=self.request.user)
+        return Contact.objects.filter(owner=self.request.user).order_by('id')
