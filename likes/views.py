3f87fb4b864e7efa4a30847a80d19c5906f7c9ba
Linus Johansson <golfarenlj@gmail.com>
2024-07-28 10:02:38 +0000

    Refactor LikeListCreateView to order queryset by created_at

--- likes/views.py
+++ likes/views.py
@@ -5,15 +5,23 @@ from api_blog.permissions import IsOwnerOrReadOnly
 
 
 class LikeListCreateView(generics.ListCreateAPIView):
+    """
+    View for listing and creating likes.
+    """
+
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    queryset = Like.objects.all().order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    View for retrieving and deleting a like.
+    """
+
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
