574de325963dc785a1d7b4da05e3c67e6cca3158
Linus Johansson <golfarenlj@gmail.com>
2024-07-29 22:57:10 +0000

    Refactor BookmarkSerializer to handle existing bookmarks during creation

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -3,6 +3,10 @@ from .models import Bookmark
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Bookmark model.
+    """
+
     owner = serializers.ReadOnlyField(source='owner.username')
     post_content = serializers.ReadOnlyField(source='post.content')
     post_owner = serializers.ReadOnlyField(source='post.owner.username')
@@ -23,16 +27,11 @@ class BookmarkSerializer(serializers.ModelSerializer):
             'created_at',
         ]
 
-    def validate(self, data):
-        owner = self.context['request'].user
-        post = data.get('post')
-        if Bookmark.objects.filter(owner=owner, post=post).exists():
-            existing_bookmark = Bookmark.objects.get(owner=owner, post=post)
-            data['existing_bookmark'] = existing_bookmark
-        return data
-
     def create(self, validated_data):
-        if 'existing_bookmark' in validated_data:
-            return validated_data['existing_bookmark']
         validated_data['owner'] = self.context['request'].user
-        return super().create(validated_data)
+        try:
+            return super().create(validated_data)
+        except:
+            return Bookmark.objects.get(
+                owner=validated_data['owner'], post=validated_data['post']
+            )
