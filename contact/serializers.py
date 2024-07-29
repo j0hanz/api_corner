4efd8a608cd9288c2df2b445a07912caa5c1f1b5
Linus Johansson <golfarenlj@gmail.com>
2024-07-29 22:22:38 +0000

    refactor ContactSerializer to make 'owner' and 'created_at' fields read-only

--- contact/serializers.py
+++ contact/serializers.py
@@ -1,23 +1,5 @@
 from rest_framework import serializers
 from .models import Contact
-from datetime import datetime, timezone, timedelta
-
-
-def shortnaturaltime(value):
-    """
-    Return a human-readable string representing the time delta from now to the given value.
-    """
-    now = datetime.now(timezone.utc)
-    delta = now - value
-
-    if delta < timedelta(minutes=1):
-        return 'just now'
-    elif delta < timedelta(hours=1):
-        return f'{int(delta.total_seconds() // 60)}m'
-    elif delta < timedelta(days=1):
-        return f'{int(delta.total_seconds() // 3600)}h'
-    else:
-        return f'{delta.days}d'
 
 
 class ContactSerializer(serializers.ModelSerializer):
