b67ba3087ecdc7572be940f6c2b32709cca13a14
Linus Johansson <golfarenlj@gmail.com>
2024-07-29 22:57:02 +0000

    Refactor Bookmark model to update owner foreign key related name and add ordering

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,22 +5,22 @@ from posts.models import Post
 
 class Bookmark(models.Model):
     """
-    Model for users to bookmark specific posts.
+    Bookmark model, representing a bookmark created by a user.
     """
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='bookmarks'
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='bookmarks'
     )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        ordering = ['-created_at']
         constraints = [
             models.UniqueConstraint(
                 fields=['owner', 'post'], name='unique_bookmark'
             )
         ]
-        ordering = ['-created_at']
 
     def __str__(self):
         return f'{self.owner} bookmarked {self.post}'
