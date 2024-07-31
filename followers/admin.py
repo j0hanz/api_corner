3f8ae3feb44e25f1a5b2521d2e22565b25eb94fd
Linus Johansson <golfarenlj@gmail.com>
2024-07-31 19:31:44 +0000

    Refactor migrations for followers, comments, likes, bookmarks, and posts

---
+++ followers/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Follower
+
+
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'followed', 'created_at')
+    search_fields = ('owner__username', 'followed__username')
+    list_filter = ('created_at',)
